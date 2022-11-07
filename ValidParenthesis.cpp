3a8a10aa43f2bcbb2d3bebc65983e9d128f78614
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2022-11-07 10:58:02 +0530

    20:easy

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,32 @@
+//20. valid paranthesis
+//level:easy
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(int i=0;i<s.length();i++){
+            if(st.empty())
+             st.push(s[i]);
+
+             else if(s[i]==')'){
+                 if(st.top()!='(') return false;
+                 st.pop();
+             }
+
+              else if(s[i]=='}'){
+                 if(st.top()!='{') return false;
+                 st.pop();
+             }
+
+              else if(s[i]==']'){
+                 if(st.top()!='[') return false;
+                 st.pop();
+             }
+
+             else st.push(s[i]);
+        }
+       if(st.empty()) return true;
+       return false; 
+    }
+};
