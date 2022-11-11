e68150570fded9a92f7e261191f673a47d6a2ef0
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2022-11-11 21:57:53 +0530

    Create TransposeMatrix.cpp

---
+++ TransposeMatrix.cpp
@@ -0,0 +1,17 @@
+//867 Transpose Matrix
+//level:easy
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> ans(n,vector<int>(m));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans[j][i]=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
