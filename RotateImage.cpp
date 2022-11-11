39e91ad256971b895283361345e582649012bd3c
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2022-11-11 22:33:26 +0530

    48;medium

---
+++ RotateImage.cpp
@@ -0,0 +1,16 @@
+//48 rotate image
+//level:medium
+//approach: 1. transpose of matrix
+//            2. swapping of columns
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n=matrix[0].size();
+        int i=0; 
+        int j=n-1;
+
+        
+    }
+};
