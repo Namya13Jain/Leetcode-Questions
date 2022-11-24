8acd265281fa2c19dbcd3e76be921c1e7c505392
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2022-11-24 11:18:31 +0530

    94:easy

---
+++ InorderTraversal.cpp
@@ -0,0 +1,24 @@
+//94 Binary tree inorder traversal
+//level: easy
+class Solution {
+public:
+    vector<int> ans;
+    void inorder(TreeNode *root)
+    {
+        if(root==NULL) return ;
+        
+            if(root->left)
+            inorder(root->left);
+
+            ans.push_back(root->val);
+
+            if(root->right)
+            inorder(root->right);
+        
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+       
+        inorder(root);
+        return ans;
+    }
+};
